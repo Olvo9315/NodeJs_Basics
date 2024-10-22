91c2fabac490132e3363ec213f78408311e443cb
olvo <alexvolenko9315@gmail.com>
2024-10-22 16:45:00 +0200

    add routers

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    
-    res.end('Hello from my server!\n');
+    console.log('Запрос получен');
+
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+        res.end('<a href="/about">Перейти на страницу обо мне</a>');
+    } 
+    else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+        res.end('<a href="/">Перейти на главную страницу</a>');
+    } 
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/html; charset=UTF-8' });
+        res.end('<h1>Страница 404</h1>');
+    }
 });
 
 const PORT = 3000;
